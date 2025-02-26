d286c5829293d7f886e82f11829c1b1452bb14c2
Adri1296 <oscardu31@gmail.com>
2025-02-26 15:52:10 -0600

    Update anim.js

--- anim.js
+++ anim.js
@@ -4,7 +4,7 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "Cada vez que me levanto", time: 1 },
+  { text: "Cada vez que me levanto", time: 20 },
   { text: "Y veo que a mi lado estás", time: 6 },
   { text: "Me siento renovado", time: 8 },
   { text: "Y me siento aniquilado", time: 10},
